4492851878c5faee9c20b7f52b35e83c85980c37
sraff123 <muhasraf321aco@gmail.com>
2021-11-19 08:22:31 +0700

    ini versi pertama saya

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <div class="container">
+
+        <!-- ini adalah pembuka navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Asraf</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Hobi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cita-Cita</a>
+            </li>
+        </ul>
+    </li>
+    
+</ul>
+      <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+</div>
+<!-- ini adalah penutup navbar-->
+<!-- content1-->
+<div class="container">
+  <div class="row">
+    <div class="col-8">
+      
+      <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/asraf.png" 
+  class="card-img-top"
+  style="width: 9rem;">
+  <div class="card-body">
+    <p class="card-text">asraf</p>
+  </div>
+</div>  
+    </div>
+    <div class="col-4">
+      <h1>Selamat datang di website saya</h1>
+      <p>Perkenalkan saya muhammad asraf biasa di panggil asraf,hobi saya main futsal dan berenang
+          ,saya benci menunggu dan benci virtual,dan paling benci covid
+          saya ahli main voly,dan bermain game
+      </p>
+    </div>
+   
+  </div>
+</div>
+</div>
+<!-- ini adalah pembungkus-->
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <!-- link javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
